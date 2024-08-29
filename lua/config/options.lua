a3294a18f9fe4e79321ea0ef8af140b47f3cc906
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-08-28 20:38:17 -0700

    fix: mapleader is now \ instead of a space
    Was really annoying have to set in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.g.mapleader = "\\"
